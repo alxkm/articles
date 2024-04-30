1db18450b8c024f9cc35c5a2c67241385f437624
alx <alx@alx.com>
2024-04-30 23:59:40 +0200

    3. Knapsack

---
+++ src/main/java/org/alx/article/_3_knapsack/Item.java
@@ -0,0 +1,11 @@
+package org.alx.article._3_knapsack;
+
+class Item {
+    int weight;
+    int value;
+
+    public Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+}
