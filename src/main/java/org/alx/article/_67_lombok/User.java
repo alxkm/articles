a042be59b9a50fdd3a65f668f999ac32ce623689
Alex Klymenko <alx@alx.com>
2024-08-01 22:04:41 +0200

    67. lombok

---
+++ src/main/java/org/alx/article/_67_lombok/User.java
@@ -0,0 +1,45 @@
+package org.alx.article._67_lombok;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
---
+++ src/main/java/org/alx/article/_67_lombok/UserSimplified.java
@@ -0,0 +1,16 @@
+package org.alx.article._67_lombok;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserSimplified {
+    int id;
+    String name;
+}
